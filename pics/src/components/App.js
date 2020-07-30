0e7c4003e5538ec3d066ff9aeacc873e6c00fd14
Sheetal Ravindran <sheetalpr30@gmail.com>
2020-07-30 13:49:25 +0530

    Calling API

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization:
+          'Client-ID E5wbG_GmvtqYDk_VZYfdZZMF2Hv3o9Xp4LLcbmNQGDI',
+      },
+    });
   }
 
   render() {
